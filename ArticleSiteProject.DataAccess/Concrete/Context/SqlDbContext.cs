c20e62d4b8dbf809524b640249e752e85dc47a35
Berkay Türk <berkayturk5481@gmail.com>
2022-07-01 12:35:20 +0300

    Data Access katmanı icerisindeki SqlDbContext sınıfı icerisinde veri tabanı baglantı adresi ve DbSet propertyleri tanımlandı

--- ArticleSiteProject.DataAccess/Concrete/Context/SqlDbContext.cs
+++ ArticleSiteProject.DataAccess/Concrete/Context/SqlDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArticleSiteProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace ArticleSiteProject.DataAccess.Concrete.Context
 {
     public class SqlDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-R443NGM;database=ArticleSiteProject;integrated security=true;");
+        }
+
+        public DbSet<Article> Articles { get; set; }
     }
 }
