e49a24290f90c58250ad38b656a8e3e2bd5b2fde
Berkay Türk <berkayturk5481@gmail.com>
2022-07-01 13:04:57 +0300

    EFArticleRepository isminde Entity Frame Repository'i olusturuldu

---
+++ ArticleSiteProject.DataAccess/Concrete/EntityFramework/EFArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using ArticleSiteProject.DataAccess.Abstract;
+using ArticleSiteProject.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleSiteProject.DataAccess.Concrete.EntityFramework
+{
+    public class EFArticleRepository : ArticleRepository, IArticleDAL
+    {
+    }
+}
