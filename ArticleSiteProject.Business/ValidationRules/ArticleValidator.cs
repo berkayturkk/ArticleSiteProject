15ca93f48126266a4b82ca6b1d48e574e10e219f
Berkay Türk <berkayturk5481@gmail.com>
2022-07-01 13:16:50 +0300

    FluentValidation paketi projeye eklendi ve ValidationRules klasörü olusturuldu

---
+++ ArticleSiteProject.Business/ValidationRules/ArticleValidator.cs
@@ -0,0 +1,14 @@
+﻿using ArticleSiteProject.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleSiteProject.Business.ValidationRules
+{
+    public class ArticleValidator : AbstractValidator<Article>
+    {
+    }
+}
