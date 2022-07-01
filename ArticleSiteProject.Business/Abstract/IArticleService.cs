3a6e099bc583bb037500a31a85893e120bdc92eb
Berkay Türk <berkayturk5481@gmail.com>
2022-07-01 13:14:15 +0300

    Business katmanı icerisinde IArticleService adında interface service eklendi

---
+++ ArticleSiteProject.Business/Abstract/IArticleService.cs
@@ -0,0 +1,18 @@
+﻿using ArticleSiteProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleSiteProject.Business.Abstract
+{
+    public interface IArticleService
+    {
+        List<Article> GetAllArticles();
+        Article GetArticleById(int id);
+        Article CreateArticle(Article article);
+        Article UpdateArticle(Article article);
+        void DeleteArticle(int id);
+    }
+}
